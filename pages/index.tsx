c897d2b632626ecf5428097a20c7d29f73d67e1d
@oumar1994 <oumarfilston@gmail.com>
2023-02-19 03:00:33 -0800

    create feed section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Sidebar from "../components/Sidebar"
-
+import Feed from "../components/Feed"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <main className='flex min-h-screen max-w-7xl mx-auto '>
         <Sidebar/>
+        <Feed/>
       </main>
     </div>
   )
