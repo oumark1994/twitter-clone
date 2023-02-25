213309f8f0b01fd12d882f5b2f0c869e5f467430
@oumar1994 <oumarfilston@gmail.com>
2023-02-24 19:13:46 -0800

    add news section

---
+++ components/News.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const News = ({article}) => {
+  return <a href={article.url} target="_blank">
+    <div  className='flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200'>
+      <div className='space-y-0.5'>
+        <h6 className='text-sm font-bold '>{article.title}</h6>
+        <p className='text-sm font-medium text-gray-500 '>{article.source.name}</p>
+      </div>
+      <img className='rounded-full' width="70" src={article.urlToImage} alt=""/>
+        
+    </div>
+    </a>
+  
+}
+
+export default News
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,9 @@ import { Inter } from '@next/font/google'
 import Sidebar from "../components/Sidebar"
 import Feed from "../components/Feed"
 const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+import Widgets from "../components/Widgets"
+export default function Home({newsResults}) {
+  // console.log(newsResults.articles)
   return (
     <div>
       <Head>
@@ -14,10 +15,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='flex min-h-screen max-w-7xl mx-auto '>
-        <Sidebar/>
+      <main className='flex min-h-screen mx-auto '>
+        <Sidebar />
         <Feed/>
+        <Widgets newsResults={newsResults.articles}/>
       </main>
     </div>
   )
+}
+//https://saurav.tech/NewsAPI/top-headlines/category/business/us.json
+export async function getServerSideProps(){
+ const newsResults = await fetch('https://saurav.tech/NewsAPI/top-headlines/category/business/us.json').then((res)=>res.json());
+return {
+  props:{
+    newsResults,
+  }
+}
 }
