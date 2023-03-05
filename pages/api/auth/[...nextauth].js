4f1addecf7902b4f2bf7562e28d8507740622ec7
@oumar1994 <oumarfilston@gmail.com>
2023-03-05 01:55:40 -0800

    complete the signing pageE

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,11 +5,14 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOLE_CLIENT_ID,
+      clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
+  pages:{
+    signin: "/auth/signin"
+  }
 }
 
 export default NextAuth(authOptions)
